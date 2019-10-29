27305a585c7f61c01eb69417543d51aca903f2c2
Fernando Mendes <fernando_moreiram@hotmail.com>
2019-10-28 22:29:07 -0300

    Start
    Inicio do Prj no Git

---
+++ src/main/java/teste/sambatech/DesafioApi/App.java
@@ -0,0 +1,13 @@
+package teste.sambatech.DesafioApi;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/teste/sambatech/DesafioApi/AppTest.java
@@ -0,0 +1,20 @@
+package teste.sambatech.DesafioApi;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
